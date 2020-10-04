48b1e931063cce5c4c255fdb50f0aece98110f15
sk <lylapp@163.com>
2020-10-04 21:56:43 +0800

    Initial Commit

---
+++ MyPay/MyPay.h
@@ -0,0 +1,12 @@
+//
+//  MyPay.h
+//  MyPay
+//
+//  Created by sk on 2020/10/4.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyPay : NSObject
+
+@end
---
+++ MyPay/MyPay.m
@@ -0,0 +1,12 @@
+//
+//  MyPay.m
+//  MyPay
+//
+//  Created by sk on 2020/10/4.
+//
+
+#import "MyPay.h"
+
+@implementation MyPay
+
+@end
